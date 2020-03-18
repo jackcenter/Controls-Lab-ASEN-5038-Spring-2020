875be7a5e30e3c22fa422728267bdfaca38c641b
Jack Center <jace3373@gmail.com>
2020-03-18 14:03:42 -0600

    sort of finalized SI for dTheta

--- Joint_Module/Joint_SI_Dtheta.m
+++ Joint_Module/Joint_SI_Dtheta.m
@@ -4,12 +4,12 @@ load('Joint_SI_CL_40s_dtheta_Data.mat')
 load('Joint_Init_Data.mat')
 load('ID_parameters.mat')
 
-R = fft(r);
+R = fft(u);
 dTheta_tf = fft(dTheta);
 
 index1 = find(F<200 * T/(2*pi)); 
 G = 0*dTheta_tf;
-G(index1) = dTheta_tf(index1)./R(index1);
+G(index1) = dTheta_tf(index1)./(R(index1)+dTheta_tf(index1));
 G(index1) = abs(G(index1));
 G_log = 20*log10(G);
 plot_index = find(G_log < 50);
@@ -30,13 +30,13 @@ s = tf('s');
 Index_id2 = find(G_log(2:end) < K_3dB);
 Index_id2 = Index_id2(1)+20;
 omega1 = F(Index_id2);
-omega1 = 1;
-omega2 = 1;
-omega3 = 1;
+omega1 = 28.3;
+omega2 = 28.3;
+omega3 = 180;
 
 eta = 0.02;
-% H_at = db2mag(K)/Ks_J2*(s^2 + B2_J2*s + Ks_J2) / ((s/omega1 + 1)*(s/omega2 + 1) *(s/omega3 + 1));
-H_at = (s^2 + eta*60*s + 900) / (s^3 + s^2 + s + 1);
+H_at = db2mag(K)/Ks_J2*(s^2 + B2_J2*s + Ks_J2) / ((s/omega1 + 1)*(s/omega2 + 1) *(s/omega3 + 1));
+H_at = .0008*(s^2 + eta*56.6*s + 800) / ((s/omega1 + 1)*(s/omega2 + 1) *(s/omega3 + 1));
 bodemag(H_at)
 hold off
 
